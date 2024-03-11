93bbe449c9988b766b2b1db50f11f51abefd952e
mariaportilla <80787979+mariaportilla@users.noreply.github.com>
2024-03-11 11:16:46 -0500

    four_commit

--- app/academics/models.py
+++ app/academics/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -6,3 +7,8 @@ class User(models.Model):
     firstname = models.CharField(max_length =20)
     lastname = models.CharField(max_length=20)
     age = models.CharField()
+    ident_number = models.CharField(max_length= 12, blank=True)
+    created_at= models.DateTimeField(default=datetime.datetime.now())
+    updated_at= models.DateTimeField(default=datetime.datetime.now())
+    delete_at= models.DateTimeField(null=True, blank=True)
+
