d216d1e3927866e5b03ae33eda2f9108385589a1
mariaportilla <80787979+mariaportilla@users.noreply.github.com>
2024-03-15 17:17:16 -0500

    five_commit

--- app/academics/models.py
+++ app/academics/models.py
@@ -2,8 +2,19 @@ from django.db import models
 import datetime
 
 # Create your models here.
+#model.DataTimeField(auto_now=True, null=false)=> updates_at
+#model.DataTimeField(auto_now_add=True, null=false)=> Desfault now()
 
 class User(models.Model):
+    
+    email=models.EmailField(null=True, blank=True)
+    password= models.CharField(null=True, blank=True)
+    status=models.BooleanField(null=True, blank=True, default=True)
+    created_at= models.DateTimeField(default=datetime.datetime.now())
+    updated_at= models.DateTimeField(default=datetime.datetime.now())
+    delete_at= models.DateTimeField(null=True, blank=True)
+
+class Person(models.Model):
     firstname = models.CharField(max_length =20)
     lastname = models.CharField(max_length=20)
     age = models.CharField()
@@ -11,4 +22,3 @@ class User(models.Model):
     created_at= models.DateTimeField(default=datetime.datetime.now())
     updated_at= models.DateTimeField(default=datetime.datetime.now())
     delete_at= models.DateTimeField(null=True, blank=True)
-
